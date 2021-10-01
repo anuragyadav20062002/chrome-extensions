3f8a7907a498030ff1ceffaa626ebf8d1c4ea388
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-10-01 22:15:50 +0530

    added api call to script.js

--- word-of-the-day/background.js
+++ word-of-the-day/background.js
@@ -1,13 +1,16 @@
-
-
 //Listen for messages
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-
-  if(msg.name == "message"){
+  if (msg.name == "fetchWords") {
+    //Wait for Response
+    const apiKey = "Our-apiKey"
+    const datestr = new Date().toISOString().slice(0, 10)
+    const apiCall =
+      "https://api.wordnik.com/v4/words.json/wordOfTheDay?date=" +
+      datestr +
+      "&api_key=" +
+      apiKey
 
     //Send response
-    response({text: "This is a response..."});
-
+    response({ api: apiCall, date: datestr })
   }
-
-});
+})
--- word-of-the-day/script.js
+++ word-of-the-day/script.js
@@ -1,12 +1,4 @@
-
-
 //Send Message To Background
-chrome.runtime.sendMessage({name: "message"}, (response) => {
-  //Wait for Response
-  const apiKey = 'Our-apiKey'
-  const datestr = new Date().toISOSstring()
-  const apiCall = 'https://api.wordnik.com/v4/words.json/wordOfTheDay?date='+datestr+'&api_key='+apiKey
-  //Update Display on Content Script
-  document.querySelector('body').innerHTML=response.text;
-
-});
+chrome.runtime.sendMessage({ name: "fetchWords" }, (response) => {
+  console.log(response)
+})
